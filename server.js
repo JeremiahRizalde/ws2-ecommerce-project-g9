722eeeb03abefdeb5a7be435bf2173ab7103e336
JeremiahRizalde <20232055@s.ubaguio.edu>
2025-09-09 11:54:47 +0800

    Lesson 11: Add logout and session timeout

--- server.js
+++ server.js
@@ -3,20 +3,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+const session = require('express-session'); // Added for user sessions
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(` Server running on port ${PORT}`);
-});
-
-
-//const port = 3000;
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
+
+// Session setup
+app.use(session({
+    secret: process.env.SESSION_SECRET || "default_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, // set to true only if using HTTPS
+        maxAge: 2 * 60 * 1000 // 15 minutes (in milliseconds)
+    }
+}));
 
 //Routes
 const indexRoute = require('./routes/index');
@@ -29,6 +35,10 @@ app.use('/users', usersRoute);
 //MongoDB Setup
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
+
+// Expose client & dbName to routes
+app.locals.client = client;
+app.locals.dbName = process.env.DB_NAME || "ecommerceDB";
 
 async function main() {
     try{
